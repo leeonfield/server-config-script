1283682e4dd3b534d42d40b12895294531340a1e
leeon <leeonfield@foxmail.com>
2017-07-12 21:10:13 +0800

    install nginx and node script

---
+++ centos/installnginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum install epel-release
+
+yum install nginx
+
+systemctl start nginx
+
+
---
+++ centos/installnode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum install node
+
+
+node --version
+
+yum install npm
+
+
+npm --version
+
+
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
